a6bf187f332934bf0f8e6a2eed1b6953c1da6817
vaporkev-e13 <67335150+vaporkev-e13@users.noreply.github.com>
2020-07-16 11:38:38 +0700

    Project web e-learning

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Learningfy</title>
+ <!-- require meta tags -->
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shink-to-fit-no">
+
+  <title>Learningfy</title>
+
+  <!-- bootstrap css -->
+  <link rel="stylesheet" type="text/css" href="./assets/bootstrap/bootstrap.css">
+
+  <!-- my css -->
+  <link href="assets/custom/style.css?version=<?php echo filemtime('./assets/custom/style.css'); ?>" rel="stylesheet">
+
+</head>
+<body>
+
+
+ <!-- navbar -->
+
+ <nav class="navbar navbar-expand-md navbar-light">
+  <div class="container">
+   <a class="navbar-brand" href="">Learningfy</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+    <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="menu">
+    <div class="navbar-nav ml-auto">
+     <a class="nav-item nav-link active" href="">Beranda<span class="sr-only">(current)</span></a>
+     <a class="nav-item nav-link" href="">SIGN-IN</a>
+    </div>
+   </div>
+  </div>
+ </nav>
+
+ <!-- end navbar -->
+
+
+ <!-- jumbotron -->
+ <div class="jumbotron jumbotron-fluid" style="background-image: url(assets/img/top-1.jpg)">
+  <div class="container">
+   <h1 class="">Ingin E-learning jadi <span>mudah?</span><br> di <span>Learningfy</span> tempatnya</h1>
+   <a href="" class="btn btn-primary tombol">SIGN-IN</a>
+  </div>
+ </div>
+ <!-- end jumbotron -->
+
+
+
+ <!-- container -->
+ <div class="container">
+ <!-- info panel -->
+ 
+  <div class="row justify-content-center">
+   <div class="col-10 info-panel">
+    <div class="row">
+     <div class="col-lg">
+      <img src="assets/img/undraw_video_files_fu10.png" alt="Pesawat" class="float-left">
+      <h4>Akses Materi</h4>
+      <p>Jangkau materi kapan saja dan dimana saja.
+      </p>
+     </div>
+     <div class="col-lg">
+      <img src="assets/img/undraw_fill_forms_yltj.png" alt="Kereta" class="float-left">
+      <h4>Akses Latihan</h4>
+      <p>Kerjakan latihan materi dimanapun kamu mau.</p>
+     </div>
+     <div class="col-lg">
+      <img src="assets/img/undraw_online_discussion_5wgl.png" alt="Gembok" class="float-left">
+      <h4>Terkoneksi dengan forum</h4>
+      <p>Jangkau interaksi dengan kelas, teman, dan guru dengan mudah.</p>
+     </div>
+    </div>
+   </div>
+  </div>
+ 
+ <!-- end info panel -->
+ 
+
+ <!-- deskripsi -->
+ 
+  <div class="row deskripsi">
+   <div class="row">
+    <div class="col-lg">
+     <img src="assets/img/undraw_teaching_f1cm.png" class="img-fluid">
+    </div>
+    <div class="col-lg">
+     <h3><span>Kemudahan</span> belajar dengan <span>E-learning</span></h3>
+     <p>Proses belajar mengajar dalam e-learning bisa dilakukan oleh siapapun, dimana pun, dan kapanpun.</p>
+     <a href="" class="btn btn-primary tombol">SIGN-IN</a>
+     <hr>
+    </div>
+   </div>
+   <div class="m-5">
+   </div>
+   <div class="row">
+    <div class="col-lg">
+     <h3>mengakses <span>materi</span> pilihan di <span>Learningfy</span></h3>
+     <p>Dapatkan berbagai materi berupa artikel, dokumen, dan video yang bahkan bisa diunduh. megerjakan tugas, latihan, dan ikuti diskusi forum.</p>
+     <a href="" class="btn btn-primary tombol">SIGN-IN</a>
+    </div>
+    <div class="col-lg">
+     <img src="assets/img/undraw_online_learning_ao11.png" class="img-fluid">
+     <hr>
+    </div>
+   </div>
+   <div class="m-5">
+   </div>
+  </div>
+ 
+ <!-- end deskripsi -->
+
+
+ <!-- footer -->
+  <div class="row footer">
+   <div class="col text-center text-capitalize">
+    <p>&copy;2020 all rights reserved. </p>
+   </div>
+  </div>
+ <!-- end footer -->
+ </div>
+ <!-- end container -->
+
+
+
+ <!-- optional javascript -->
+ <!--jquery, pooper.js, bootstrap.js-->
+ <script src="./assets/jquery/jquery-3.5.1.min.js"></script>
+ <script src="./assets/js/popper.min.js"></script>
+ <script src="./assets/js/bootstrap.js"></script>
+</body>
+</html>
